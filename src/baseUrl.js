b4ea3537e9a0e41de4dac9a3d7e22f03092e2dea
LehruAngela <angelalehru@gmail.com>
2018-03-19 21:02:52 +0300

    [fix] Fix baseUrl.js

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://127.0.0.1:5000/"
-export const BASE_URL="https://yummy-recipes-api-v1.herokuapp.com"
+export const BASE_URL = 'https://yummy-recipes-api-v1.herokuapp.com';
